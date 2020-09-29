fa3ec0519dd2804bac0695ab4724c0ab088723d1
ワタル <wataru.tahara@gmail.com>
2020-09-29 18:17:36 +0900

    no message

---
+++ lesson/apollo/script.js
@@ -0,0 +1,13 @@
+$(function(){
+	$('#slick_sliders').slick({
+		autoplay:true,
+		autplayspeed:5000,
+		dots:true,
+	});
+
+	$('#use').slick({
+		// autoplay:true,
+		autplayspeed:4000,
+		arrows: true,
+	});
+});
